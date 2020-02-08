86e92e640ac5a254168143882081b7aed74ca111
AnushaPB <anusha.bishop@yale.edu>
2020-02-08 14:35:48 -0500

    pca updates

--- GPDGENCON/sc02_pt2_pcaKEN_ALLDATA.R
+++ GPDGENCON/sc02_pt2_pcaKEN_ALLDATA.R
@@ -10,44 +10,74 @@ library("gdistance")
 library("foreach")
 library("doParallel")
 library("doMC")
+library("dplyr")
 
 #Plot straight lines for first iteration of RF
-G.table <- read.table(file="/home/fas/caccone/apb56/project/GFFGENCON/ken_pca_gendf.csv", sep=",", header=T)
-
 env <- stack("/home/fas/caccone/apb56/project/GPDHABITAT/chelsa_merit_vars_kenya.tif")
-
+ext <- extent(env)
 crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") # ... add coordinate system
+
+G.table <- read.table(file="/home/fas/caccone/apb56/project/GFFGENCON/ken_pca_gendf.csv", sep=",", header=T)
+#G.table <- read.table(file="/Users/Anusha/Documents/GpdKenya/ken_pca_gendf.csv", sep=",", header=T)
+#REMOVE POINTS OUTSIDE OF EXTENT
+G.table <- subset(G.table, 
+                  long1 > ext@xmin 
+                  & long1 < ext@xmax 
+                  & lat1 > ext@ymin 
+                  & lat1 < ext@ymax 
+                  & long2 > ext@xmin 
+                  & long2 < ext@xmax 
+                  & lat2 > ext@ymin 
+                  & lat2 < ext@ymax)
 
 ###############################################
 #Plot lines as SpatialLines:
 ###############################################
 
 #Plot straight lines for first iteration of RF
+#create group of ONLY unique coordinate pairs (reduces number of lines)
+unique_coords <-  unique(G.table[,c("long1","lat1","long2","lat2")])
 
-begin.table <- G.table[,c("long1","lat1")]
-begin.coord <- begin.table
+begin.table <- unique_coords[,c("long1","lat1")]
+begin.coord <- begin.table #copy one for coords, one for df
 coordinates(begin.coord) <- c("long1", "lat1")
 
-end.table <- G.table[,c("long2","lat2")]
-end.coord <- end.table
+end.table <- unique_coords[,c("long2","lat2")]
+end.coord <- end.table #copy one for coords, one for df
 coordinates(end.coord) <- c("long2", "lat2")
-
-p <- psp(begin.table[,1], begin.table[,2], end.table[,1], end.table[,2], owin(range(c(begin.table[,1], end.table[,1])), range(c(begin.table[,2], end.table[,2]))))
-
-spatial.p <- as(p, "SpatialLines")
-proj4string(spatial.p) <- crs.geo  # define projection system of our data
-
 
 ########################################
 #Calculate mean of straight lines 
 #######################################
-StraightMean <- raster::extract(env, spatial.p, fun=mean, na.rm=TRUE)
+registerDoMC(cores=detectCores()) 
 
-StraightMeanDF <- as.data.frame(StraightMean)
+StraightMeanUniq <- foreach(r=1:nrow(begin.table), .combine='rbind', .packages=c('raster', 'gdistance')  ,   .inorder=TRUE   ) %dopar% {
+  p <- psp(begin.table[r,1], begin.table[r,2], end.table[r,1], end.table[r,2], owin(range(c(begin.table[,1], end.table[,1])), range(c(begin.table[,2], end.table[,2]))))
+  spatial.p <- as(p, "SpatialLines")
+  proj4string(spatial.p) <- crs.geo 
+  data.frame(raster::extract(env, spatial.p, fun=mean, na.rm=TRUE))
+  }
 
-StraightMeanDF$Distance <- G.table$value
+gc()
 
+StraightMeanUniqDF <- as.data.frame(StraightMeanUniq)
 
+#export table
+write.csv(StraightMeanUniqDF,"/home/fas/caccone/apb56/project/GPDGENCON/PCA/StraightMeanUniqDF.csv") 
+
+#bind unique coords to unique lines
+StraightMeanUniqDF <- cbind(unique_coords, StraightMeanUniqDF)
+
+#use left_join to merge tables by coords in order to get the distance for each pair/line
+StraightMeanDF <- left_join(StraightMeanUniqDF, G.table, by = c("long1","lat1","long2","lat2"))
+
+#subset to remove long/lat and var1/var2 and X before building models
+StraightMeanDF <- subset(StraightMeanDF, select=-c(long1,lat1,long2,lat2,Var1,Var2,X))
+
+write.csv(StraightMeanDF,"/home/fas/caccone/apb56/project/GPDGENCON/PCA/StraightMeanDF.csv")
+
+#remove any NAs before random forest
+StraightMeanDF <- StraightMeanDF[complete.cases(StraightMeanDF),]
 
 ###############################################
 #Model with all data
--- GPDGENCON/sc02_pt2_pcaKEN_ALLDATA.sh
+++ GPDGENCON/sc02_pt2_pcaKEN_ALLDATA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash                                                                                                                                        
-#SBATCH -p day                                                                                                                                     
-#SBATCH --mem=118g                                                                                                                                 
-#SBATCH -n 1 -c 8 -N 1                                                                                                                             
-#SBATCH -t 24:00:00                                                                                                                                
+#SBATCH -p bigmem                                           
+#SBATCH --mem=500g
+#SBATCH -n 1 -c 8 -N 1
+#SBATCH -t 24:00:00                                      
 #SBATCH -o /gpfs/scratch60/fas/caccone/apb56/stdout/sc02_pt2_pcaKEN_ALLDATA.sh.%J.out                                                               
 #SBATCH -e /gpfs/scratch60/fas/caccone/apb56/stderr/sc02_pt2_pcaKEN_ALLDATA.sh.%J.err                                                               
 #SBATCH --mail-type=ALL                                                                                                                            
