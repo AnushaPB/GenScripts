fe54ea71e49a317cdb9390654667bc3a620b5038
AnushaPB <anusha.bishop@yale.edu>
2020-03-16 22:23:32 -0400

    LOPOCV LCP

--- GPDGENCON/sc01_pt1_edwards_LOPOCV.R
+++ GPDGENCON/sc01_pt1_edwards_LOPOCV.R
@@ -10,7 +10,6 @@ library("foreach")
 library("doParallel")
 library("doMC")
 
-
 rmr=function(x){
   ## function to truly delete raster and temporary files associated with them
   if(class(x)=="RasterLayer"&grepl("^/tmp",x@file@name)&fromDisk(x)==T){
@@ -31,17 +30,23 @@ rm(env)
 #Plot straight lines for first iteration of RF
 G.table <- read.table(file="/home/fas/caccone/apb56/project/GPDGENCON/ken_edwards_gendf.csv", sep=",", header=T)
 
+#create sorted list of unique sites 
+sites <-  sort(unique(c(levels(G.table$Var1),levels(G.table$Var2))))
+
+#create list of points
 #Create Leave One Point Out Cross Validation (LOPOCV) data 
-for (i in levels(pop.df$Var1)){
-  testData <- pop.df[pop.df$Var1==i | pop.df$Var2==i,]
-  #write.csv(testData, paste0("/home/fas/caccone/apb56/project/GPDGENCON/Edwards/LOPOCV/testData_", i, ".csv"))
+for (i in 1:length(sites)){
+  testData <- G.table[G.table$Var1 == sites[i] | G.table$Var2 == sites[i],] #include rows where the site appears in either column (OR)
+  write.csv(testData, paste0("/home/fas/caccone/apb56/project/GPDGENCON/Edwards/LOPOCV/testData_", i, ".csv"))
   assign(paste0("testData_", i), testData)
   
-  trainData <- pop.df[pop.df$Var1!=i & pop.df$Var2!=i,]
-  #write.csv(trainData, paste0("/home/fas/caccone/apb56/project/GPDGENCON/Edwards/LOPOCV/trainData_", i, ".csv"))
+  trainData <- G.table[G.table$Var1 != sites[i] & G.table$Var2 != sites[i],] #keep rows where the Vars do NOT equal the site for BOTH Vars (&)
+  #note: needs to be &  not | because you want the conditionalf for both rows to be TRUE (both rows to NOT have the site)
+  #for ex, if you get a T for V1 (V1 does not equal site) and a F for V2 (V2 does equal site) and you want that row DISCARDED 
+  write.csv(trainData, paste0("/home/fas/caccone/apb56/project/GPDGENCON/Edwards/LOPOCV/trainData_", i, ".csv"))
   assign(paste0("trainData_", i), trainData)
   
-  print(nrow(train)+nrow(test) == nrow(pop.df)) #SHOULD BE TRUE ALWAYS
+  print(nrow(trainData)+nrow(testData) == nrow(G.table)) #SHOULD BE TRUE ALWAYS
 }
 
 save.image(file = "/home/fas/caccone/apb56/project/GPDGENCON/Edwards/LOPOCV/RF_pt1.RData")
